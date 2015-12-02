da43bceb059a0ebc53e0d236e0622092083486ba
Mohammed Shaikh <javeed.shaikh@gmail.com>
2015-12-02 02:37:38 -0500

    Initial Commit

---
+++ MemeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeDemo
+//
+//  Created by Mohammed Shaikh on 2015-12-02.
+//  Copyright © 2015 Zaytun Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
